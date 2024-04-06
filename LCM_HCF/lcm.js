b6a387bcbfd8f38b256afa427a9c7201162d588a
Vinayak <singhvinayak7111@gmail.com>
2024-04-06 11:59:59 +0530

    LCM and HCF

---
+++ LCM_HCF/lcm.js
@@ -0,0 +1,21 @@
+// program to find the LCM of two integers
+
+let hcf;
+// take input
+const number1 = 40;
+const number2 = 15;
+
+// looping from 1 to number1 and number2 to find HCF
+for (let i = 1; i <= number1 && i <= number2; i++) {
+
+    // check if is factor of both integers
+    if( number1 % i == 0 && number2 % i == 0) {
+        hcf = i;
+    }
+}
+
+// find LCM
+let lcm = (number1 * number2) / hcf;
+
+// display the hcf
+console.log(`HCF of ${number1} and ${number2} is ${lcm}.`);
